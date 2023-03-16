afca204ef4d8821d2a83fd7673b3e5adf701fd0b
Riley-King <118085747+Riley-King@users.noreply.github.com>
2023-03-17 09:32:25 +1100

    Added our files and libs

---
+++ Project/main.py
@@ -0,0 +1,24 @@
+# Import pre-existing libs
+from machine import Pin
+import time
+
+# Import custom libs/wrappers
+from libs.MotorPair import MotorPair
+from libs.EncoderPair import EncoderPair
+import libs.US
+import libs.APDS9960LITE
+import libs.ssd1306
+
+# Pins for IR sensors
+IR_l = Pin(27, Pin.IN)
+IR_r = Pin(26, Pin.IN)
+IR_c = Pin(28, Pin.IN)
+
+# Create instance of motor and encoder wrappers
+motors = MotorPair()
+encoder = EncoderPair()
+
+while True:
+    motors.duty(50, 50)
+    encoder.reset()
+    
