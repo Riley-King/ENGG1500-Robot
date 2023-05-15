a39cfe2e5bd9762d5ad12c6eefcd6a7d9130d94d
Riley-King <c3430382@uon.edu.au>
2023-05-15 17:11:42 +1000

    Removed webserver as it was pretty useless

--- states/initialise.py
+++ states/initialise.py
@@ -8,22 +8,11 @@ from libs.MotorPair import MotorPair
 from libs.EncoderPair import EncoderPair
 from libs.displayLib import Display
 from libs.APDS9960LITE import APDS9960LITE
-
-import libs.Webserver as web
-
 def state_initialise(env: dict, *args) -> str:
     # Used in delta-time calculations
     env.lastUpdated = float(time.ticks_ms())
     # Define the robots logical state
     env.robot_state = "followLine"
-    # Should the webserver be started?
-    env.doWeb = True
-
-    # Start web server if enabled
-    if env.doWeb:
-        web.loadHTMLRecurse("web", noConcatPath=True)
-        web.web_pages["/"] = web.web_pages["/index.html"]
-        env.web = web
 
     # Create I/O devices such as sensors and motors
     env.ir_pins = [Pin(19), Pin(27), Pin(28), Pin(26), Pin(22)]
--- states/updateGraphics.py
+++ states/updateGraphics.py
@@ -3,9 +3,6 @@ import time
 def state_updateGraphics(env:dict, *args:any) -> str:
 
     if float(time.ticks_ms()) > (env.lastDisplayed + 1):
-        if env.doWeb: env.web.update_webserver()
-
-
         env.display.clear()
 
         env.display.text("updateGraphics")
